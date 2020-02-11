bb44d78721f9622b3176e54cccbeb24ab6b03039
Pedro Santiago <pedroh_s_@hotmail.com>
2020-02-11 19:06:18 -0300

    TDD

--- packages/calculator-service/src/app.controller.spec.ts
+++ packages/calculator-service/src/app.controller.spec.ts
@@ -15,8 +15,29 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Health check ok!"', () => {
+      expect(appController.getHello()).toBe('Health check ok!');
+    });
+  });
+  describe('/cdb/post-fixed', () => {
+    describe('given an investment date, cdbRate and currentDate', () => {
+      it('should return a daily unit price', () => {
+        const mockConsumer = {
+          "investmentDate":"2016-11-14",
+          "cdbRate": 103.5,
+          "currentDate":"2016-11-16"
+        }
+expect(appController.cdb(mockConsumer)).toBe([
+{
+            "date": "2016-12-26",
+            "unitPrice": 1000.53397
+          },
+          {
+            "date": "2016-12-25",
+            "unitPrice": 1001.06822
+          }
+          ]);
+      });
     });
   });
 });
