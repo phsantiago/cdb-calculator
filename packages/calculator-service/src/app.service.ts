b1ae43c40300b547c0af491a8055fad56654df96
Pedro Santiago <pedroh_s_@hotmail.com>
2020-02-11 19:06:50 -0300

    post fixed cdb

--- packages/calculator-service/src/app.service.ts
+++ packages/calculator-service/src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Health check ok!';
+  }
+  calculateCDB(data): string {
+    return 'Health check ok!';
   }
 }
