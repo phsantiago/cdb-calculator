1bbe988b06c3663a7438f55d44de7fd430d845ab
Pedro Santiago <pedroh_s_@hotmail.com>
2020-02-11 22:39:32 -0300

    wip: formatting

--- packages/calculator-service/src/app.service.ts
+++ packages/calculator-service/src/app.service.ts
@@ -1,11 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import moment from 'moment';
+import marketData = require('./marketData.json');
+
+type datePrice = {
+  date: string,
+  unitPrice: number
+}
+
+type postFixedCdbDTO = {
+  investmentDate: string,
+  cdbRate: number,
+  currentDate: string,
+  price?: number
+}
+
+/* type cdbCalcResult = Array<datePrice>; */
+const calculateUnitPrice = a => a;
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Health check ok!';
   }
-  calculateCDB(data): string {
-    return 'Health check ok!';
+  calculateTCDI(CDI) {
+    return 1
+  }
+  calculateCDB({ investmentDate, currentDate })/*: cdbCalcResult*/ {
+    const momentInvestmentDate = moment(investmentDate, "YYYY-MM-DD");
+    const momentCurrentDate = moment(currentDate, "YYYY-MM-DD");
+    const treatedMarketData = marketData
+      .map(({ dtDate, ...rest }) => ({ date: moment(dtDate, "DD/MM/YYYY"), ...rest}))
+      .map(({ sSecurityName, ...rest }) => rest);
+ 
+    const marketDataSlice = treatedMarketData.filter(({ date }) => {
+      if(date.isSameOrAfter(momentInvestmentDate) && date.isSameOrBefore(momentCurrentDate)) return true
+    }) 
+
+    const sorted = marketDataSlice.sort(({date: dateA}, {date: dateB}) => dateA.unix() - dateB.unix())
+
+    const calculated = sorted.map(({ dLastTradePrice, ...rest }) => ({ unitPrice: calculateUnitPrice(dLastTradePrice), ...rest}))
+
+    const dateFormated = calculated.map(({ date, ...rest }) => ({ date: date.format('YYYY-MM-DD'), ...rest }));
+    return dateFormated;
   }
 }
+
+
+
