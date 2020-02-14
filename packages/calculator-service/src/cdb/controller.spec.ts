c09502f943d06024ffd2c041cc91ccac3cf1f227
Pedro Santiago <pedroh_s_@hotmail.com>
2020-02-14 01:26:53 -0300

    refactor

--- packages/calculator-service/src/cdb/controller.spec.ts
+++ packages/calculator-service/src/cdb/controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controller';
+import { AppService } from './service';
 import marketData from './marketData.json';
 
 jest.mock('./marketData.json', () => ([
